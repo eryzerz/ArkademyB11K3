f18fbca0b7be075c77793eeea337a135ad1c26ac
eryz <eryzero@gmail.com>
2019-07-13 18:43:21 +0700

    Modify problem_5.py

---
+++ problem_5.py
@@ -0,0 +1,28 @@
+def is_prime(num):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+    else:
+        return False
+
+
+def display_triangle(num):
+    ctr = 2
+    for i in range(num + 1):
+        for j in range(i):
+            while is_prime(ctr) == False:
+                ctr += 1
+            print(ctr, end=" ")
+            ctr += 1
+        print()
+
+
+def main():
+    level = int(input())
+    display_triangle(level)
+
+
+if __name__ == "__main__":
+    main()
